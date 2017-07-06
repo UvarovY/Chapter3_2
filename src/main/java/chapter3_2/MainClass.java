fcf37be1f6cfcb485bcb36ce172a0a7a6e82a470
UvarovY <uvarovy@gmail.com>
2017-07-06 20:24:31 +0300

    all java files

---
+++ src/main/java/chapter3_2/MainClass.java
@@ -0,0 +1,13 @@
+package chapter3_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainClass
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/pact.selenium.chap3_2/Ex1Test.java
@@ -0,0 +1,75 @@
+package pact.selenium.chap3_2;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class Ex1Test {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        //System.setProperty("webdriver.gecko.driver", "C:\\Users\\Arni\\geckodriver-v0.17.0-win64\\geckodriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Arni\\WebDrivers\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        baseUrl = "http://docs.seleniumhq.org";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testExample1() throws Exception {
+        driver.get(baseUrl + "/");
+        driver.findElement(By.linkText("Download")).click();
+        driver.findElement(By.linkText("Release Notes")).click();
+        assertEquals("SeIDE Release Notes · SeleniumHQ/selenium Wiki · GitHub", driver.getTitle());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
